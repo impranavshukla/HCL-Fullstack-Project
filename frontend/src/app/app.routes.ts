566b6657dc8a2aef3c960e1414bfade6f04fcb89
Preeti Singh <preetisingh@Preetis-MacBook-Air.local>
2026-04-16 13:02:11 +0530

    modify adminLayout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,27 @@
 import { Routes } from '@angular/router';
 import { AdminLayout } from './layout/admin-layout/admin-layout';
 import { CustomerLayout } from './layout/customer-layout/customer-layout';
+import { Login } from './shared/login/login';
+import { Signup } from './shared/signup/signup';
+import { Home } from './adminPages/home/home';
+import { Bookings } from './adminPages/bookings/bookings';
+import { Hotels } from './adminPages/hotels/hotels';
+import { Rooms } from './adminPages/rooms/rooms';
 
 export const routes: Routes = [
-
-    { path: 'admin', component: AdminLayout },
-     { path: 'customer', component: CustomerLayout }
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'signup', component: Signup },
+    { 
+        path: 'admin', 
+        component: AdminLayout,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: Home },
+            { path: 'bookings', component: Bookings },
+            { path: 'hotels', component: Hotels },
+            { path: 'rooms', component: Rooms }
+        ]
+    },
+    { path: 'customer', component: CustomerLayout }
 ];
