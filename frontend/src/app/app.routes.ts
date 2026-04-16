fb0a230e0857f5d16247d1faec97d58ff096b259
Preeti Singh <preetisingh@Preetis-MacBook-Air.local>
2026-04-16 12:03:48 +0530

    modify admin layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { AdminLayout } from './layout/admin-layout/admin-layout';
+import { CustomerLayout } from './layout/customer-layout/customer-layout';
 
 export const routes: Routes = [
 
-    
+    { path: 'admin', component: AdminLayout },
+     { path: 'customer', component: CustomerLayout }
 ];
